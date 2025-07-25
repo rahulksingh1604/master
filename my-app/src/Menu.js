60b53ad1ddcf175eaf1d404d69626a536ddbefab
rahulksingh1604 <thakur.1604@gmail.com>
2025-07-25 16:20:53 +0530

    Wipro Training Practice Day 01

---
+++ my-app/src/Menu.js
@@ -0,0 +1,26 @@
+function Menu(){
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+    </div>
+  );
+}
+
+export default Menu;
+
+export function Func1() {
+  return (
+    <div>
+      <h2>This is Function 1</h2>
+    </div>
+  );
+}
+
+
+export function Func2() {
+  return (
+    <div>
+      <h2>This is Function 2</h2>
+    </div>
+  );
+}
