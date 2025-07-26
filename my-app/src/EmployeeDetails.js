5502b8444f8b2399cca01f231f5d53e56a7e2dfa
rahulksingh1604 <thakur.1604@gmail.com>
2025-07-26 14:04:23 +0530

    Assignment 01

---
+++ my-app/src/EmployeeDetails.js
@@ -0,0 +1,19 @@
+function EmployeeDetails(){
+  return (
+    <div>
+      <h1>Employee Details</h1>
+      <label>Name: </label>
+      Rahul
+      <br />
+      <label>Age: </label>
+      30
+      <br />
+      <label>Department: </label>
+      Sales
+      <br />
+      <label style={{ color: "red" , fontWeight: "bold"}}>Note: Employee details using JSX.</label>
+    </div>
+  );
+}
+
+export default EmployeeDetails;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,19 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Menu,{Func1,Func2} from './Menu';
+import EmployeeDetails  from './EmployeeDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let breakElement = React.createElement("br", {});
+let HWElement = React.createElement("div",{},"Hello World!");
+let NoteElement = React.createElement("label", { style: { color: "red", fontWeight: "bold" } }, "Note: Hello World using React.");
 root.render(<div>
-  <Menu />
-  <Func1 />
-  <Func2 />
-  </div>
+  <EmployeeDetails />
+  { breakElement }
+  { HWElement }
+  { NoteElement }
+</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
